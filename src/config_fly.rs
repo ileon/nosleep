c960149305d3992ffe0faa199cac75d1962db0bf
FLY <story@foxmail.com>
2024-12-30 09:54:49 +0800

    init

---
+++ src/config_fly.rs
@@ -0,0 +1,27 @@
+use std::sync::LazyLock;
+
+use config::Config;
+use serde::Deserialize;
+
+/// Global config
+pub static CONFIG: LazyLock<MyConfig> = LazyLock::new(|| config_init());
+
+/// Initialize config
+fn config_init() -> MyConfig {
+    //settings builder
+    let settings = Config::builder()
+        // 添加配置文件
+        .add_source(config::File::with_name("./config.toml"))
+        .build()
+        .expect("settings build failed");
+    //反序列化设置，生成设置的结构体
+    let config: MyConfig = settings
+        .try_deserialize()
+        .expect("Deserialize config failed");
+    config
+}
+
+#[derive(Debug, Deserialize)]
+pub struct MyConfig {
+    pub interval: u64,
+}
---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::time::Duration;
+
+use config_fly::CONFIG;
+use enigo::{Coordinate, Enigo, Mouse, Settings};
+use log::info;
+use rustlib::log_fly;
+pub mod config_fly;
+use tokio::{self, select, sync::mpsc::channel, time::sleep};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    // Initialize logger
+    log_fly::log_init();
+    // Get interval from config
+    let interval = CONFIG.interval;
+    // Setup Ctrl-C handler
+    let (tx, mut rx) = channel::<()>(1);
+    ctrlc2::set_async_handler(async move {
+        tx.send(())
+            .await
+            .expect("Could not send signal on channel.");
+    })
+    .await;
+    // Setup Enigo
+    let mut enigo = Enigo::new(&Settings::default()).unwrap();
+    loop {
+        // Move mouse
+        enigo.move_mouse(1, 1, Coordinate::Rel).unwrap();
+        // Wait for a while
+        sleep(Duration::from_millis(10)).await;
+        // Move mouse back
+        enigo.move_mouse(-1, -1, Coordinate::Rel).unwrap();
+        // Wait for a while for next move
+        let sleep_future = sleep(Duration::from_secs(interval));
+        // Check if Ctrl-C was pressed
+        select! {
+            // Wait for a while for next move
+            _=sleep_future=>{},
+            // Check if Ctrl-C was pressed
+            _=rx.recv()=>{
+                info!("Ctrl-C pressed, exiting...");
+                break;
+            }
+        }
+    }
+}
