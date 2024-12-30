5b3f7d1bd81783ab0450d57b30926445671b10b6
FLY <story@foxmail.com>
2024-12-30 13:44:29 +0800

    feat: 优化代码结构并添加错误处理
    - 引入 anyhow 库用于错误处理
    - 重构 main 函数，分离逻辑到 run_loop 函数
    - 添加错误处理逻辑，包括 Enigo 初始化和配置间隔为 0 的情况
    - 优化 Ctrl-C 处理逻辑
    - 更新配置文件中的默认间隔值

--- src/main.rs
+++ src/main.rs
@@ -2,43 +2,74 @@ use std::time::Duration;
 
 use config_fly::CONFIG;
 use enigo::{Coordinate, Enigo, Mouse, Settings};
-use log::info;
+use log::error;
 // use rustlib::log_fly;
 pub mod config_fly;
-use tokio::{self, select, sync::mpsc::channel, time::sleep};
+use tokio::{
+    self, select,
+    sync::mpsc::{self, channel},
+    time::sleep,
+};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     // Initialize logger
     // log_fly::log_init();
-    // Get interval from config
-    let interval = CONFIG.interval;
+
     // Setup Ctrl-C handler
-    let (tx, mut rx) = channel::<()>(1);
+    let (tx, rx) = channel::<()>(1);
     ctrlc2::set_async_handler(async move {
-        tx.send(())
-            .await
-            .expect("Could not send signal on channel.");
+        if let Err(e) = tx.send(()).await {
+            error!("Error sending message: {}", e);
+        }
     })
     .await;
-    // Setup Enigo
-    let mut enigo = Enigo::new(&Settings::default()).unwrap();
+
+    // Setup Enigo to move mouse
+    let mut enigo = match Enigo::new(&Settings::default()) {
+        // if ok, continue
+        Ok(enigo) => enigo,
+        // If error, exit
+        Err(e) => {
+            error!("Error creating Enigo: {}", e);
+            return;
+        }
+    };
+
+    // Run the loop, if error, print error
+    if let Err(e) = run_loop(&mut enigo, rx).await {
+        println!("Error running loop: {}", e);
+    }
+}
+
+/// Run the loop
+async fn run_loop(enigo: &mut Enigo, mut rx: mpsc::Receiver<()>) -> anyhow::Result<()> {
+    // Get interval from config
+    let interval = CONFIG.interval;
+    // Check if interval is 0
+    if interval == 0 {
+        return Err(anyhow::anyhow!("The interval can not be 0 in config.toml"));
+    }
+    // Count for how many times the mouse has moved
     let mut count = 1u64;
     loop {
-        // Print the count
+        // Print the time, count and interval
         println!(
             "{} --> The {}-th time move. Every {} seconds. Press CTRL+C to exit this program.",
             chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
             count,
             interval
         );
+        // Increase the count
         count += 1;
+
         // Move mouse
         enigo.move_mouse(1, 1, Coordinate::Rel).unwrap();
         // Wait for a while
         sleep(Duration::from_millis(10)).await;
         // Move mouse back
         enigo.move_mouse(-1, -1, Coordinate::Rel).unwrap();
+
         // Wait for a while for next move
         let sleep_future = sleep(Duration::from_secs(interval));
         // Check if Ctrl-C was pressed
@@ -47,9 +78,10 @@ async fn main() {
             _=sleep_future=>{},
             // Check if Ctrl-C was pressed
             _=rx.recv()=>{
-                info!("Ctrl-C pressed, exiting...");
+                println!("Ctrl-C pressed, exiting...");
                 break;
             }
         }
     }
+    Ok(())
 }
